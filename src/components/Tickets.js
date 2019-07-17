e1e66f1fc8fdc8fcacc5e076af59444776f114ea
TATY <tatyccsilva@gmail.com>
2019-07-17 13:06:47 +0200

    Add option to create a ticket

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
+import './Tickets.css'
 
 export default class Tickets extends Component {
     renderTickets = () => {
         return this.props.tickets.map(ticket => {
             return (
                 // <Link to={`/events/${event.id}/tickets`} key={event.id}>
-                    <div key={ticket.id} className="ticket-container">
-                        <p>{ticket.userId}</p>
-                        <p>{ticket.price}</p>
-                        <p>{ticket.description}</p>
-                    </div>
+                <div key={ticket.id} className="ticket-container">
+                    <p>{ticket.author}</p>
+                    <p>{ticket.price} €</p>
+                    <p>{ticket.description}</p>
+                </div>
                 // </Link >
             )
         })
@@ -17,10 +18,15 @@ export default class Tickets extends Component {
 
     render() {
         console.log('props Tickets', this.props);
-        
+
         const { tickets } = this.props
         return (
             <div>
+                <div className="title">
+                    <span>Seller</span>
+                    <span>Price</span>
+                    <span>Description</span>
+                </div>
                 {!tickets && 'Loading...'}
                 {tickets && this.renderTickets()}
             </div>
--- src/components/TicketsContainer.js
+++ src/components/TicketsContainer.js
@@ -1,22 +1,83 @@
 import React, { Component } from 'react'
-import Tickets from './Tickets'
 import { connect } from 'react-redux'
+import Tickets from './Tickets'
+import Modal from './Modal'
 import { getTickets } from '../actions/tickets.js'
-
+import './Tickets.css'
 
 class TicketsContainer extends Component {
+    state = {
+        price: '',
+        description: '',
+        pictureUrl: '',
+        openModal: false
+    }
+
     id = this.props.match.params.id
 
     componentDidMount() {
         this.props.getTickets(this.id)
     }
 
-    render() {
-        console.log('props TicketsContainer', this.id);
-        
+    onChange = (event) => {
+        const value = event.target.value
+        const key = event.target.name
+
+        this.setState({
+            [key]: value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault()
+        this.props.postTicket(
+            this.state.price,
+            this.state.description,
+            this.state.pictureUrl
+        )
+        this.setState({
+            price: '',
+            description: '',
+            pictureUrl: ''
+        })
+    }
+
+    showModal = () => {
+        this.setState({ openModal: true })
+    }
+
+    hideModal = () => {
+        this.setState({ openModal: false })
+    }
+
+    renderFormCreate = () => {
+        const { name } = this.state
+        const { description } = this.state
+        const { pictureUrl } = this.state
+
         return (
             <div>
+                <form onSubmit={this.onSubmit} className="form-create">
+                    <h3>Add a ticket: </h3>
+                    <label>Price: </label>
+                    <input onChange={this.onChange} value={name} name="name"></input>
+                    <label>Description: </label>
+                    <input onChange={this.onChange} value={description} name="description"></input>
+                    <label>Image url: </label>
+                    <input onChange={this.onChange} value={pictureUrl} name="pictureUrl"></input>
+
+                    <button>Add</button>
+                </form>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.showModal}>Create a ticket</button>
                 <Tickets tickets={this.props.tickets}/>
+                <Modal openModal={this.state.openModal} hideModal={this.hideModal} form={this.renderFormCreate} />
             </div>
         )
     }
