11a0a27e1ca7ae514233564dca84d9da6922e4c5
TATY <tatyccsilva@gmail.com>
2019-07-18 12:37:45 +0200

    Add button to close modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,11 +3,13 @@ import './Modal.css'
 
 export default class Modal extends Component {
     render() {
+        console.log('propsmodal', this.props)
         if (this.props.openModal) {
             return (
                 <div className="modal">
                     <div className="modal-main">
                         {this.props.form()}
+                        <button onClick={this.props.hideModal}>X</button>
                     </div>
                 </div>
             )
